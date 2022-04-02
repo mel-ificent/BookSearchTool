0cb9ce22d75325694c0f89f34020206cfa413871
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-04-02 11:28:47 -0400

    corrected search book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -12,9 +12,6 @@ const SavedBooks = () => {
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
   const userData = data?.me || {};
 
-  // const [userData, setUserData] = useState({});
-
-
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
